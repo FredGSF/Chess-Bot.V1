13ef815329d5af0182c126fe365317c1ef7ca74e
FredGSF <99353119+FredGSF@users.noreply.github.com>
2024-03-08 18:51:31 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,6 +13,7 @@ def find_by_css_selector(driver, css_selector):
         return None
     return element
 
+
 # wait for <wait> seconds until an element becomes available
 # change wait based on the time control of the game
 def find_by_css_selector_persist(driver, css_selector, wait=0.3):
@@ -22,38 +23,42 @@ def find_by_css_selector_persist(driver, css_selector, wait=0.3):
         element = find_by_css_selector(driver, css_selector)
     return element
 
+
 def createSVGfromBoard(board, best_move=None):
     OutputFilename = "svg/board.svg"
     if not best_move:
         svg = chess.svg.board(board)
     else:
         sq1, sq2 = best_move[0:2], best_move[2:]
-        svg = chess.svg.board(board,\
-            arrows=[chess.svg.Arrow(square2num(sq1),\
-                                    square2num(sq2), color="blue")])
-    with open(OutputFilename, 'w') as file:
+        svg = chess.svg.board(
+            board,
+            arrows=[chess.svg.Arrow(square2num(sq1), square2num(sq2), color="blue")],
+        )
+    with open(OutputFilename, "w") as file:
         file.write(svg)
     return OutputFilename
 
+
 def square2num(square):
     col, row = square[0].upper(), square[1]
-    return (int(row)-1)*8 + (ord(col)-ord('A'))
+    return (int(row) - 1) * 8 + (ord(col) - ord("A"))
+
 
 def getStockfishEnginePath():
     PATH = r"../dependencies/stockfish/"
     os_name = os.name
-    if os_name == "nt": # windows
+    if os_name == "nt":  # windows
         PATH += r"stockfish_15_x64_popcnt.exe"
-    elif os_name == "posix": # linux
+    elif os_name == "posix":  # linux
         PATH += r"stockfish-ubuntu-20.04-x86-64"
     return PATH
-    
+
+
 def getGeckodriverPath():
     PATH = r"../dependencies/geckodrivers/"
     os_name = os.name
-    if os_name == "nt": # windows
+    if os_name == "nt":  # windows
         PATH = r"geckodriver.exe"
-    elif os_name == "posix": # linux
+    elif os_name == "posix":  # linux
         PATH = r"geckodriver"
     return PATH
-    

5c3bd76169a0ed09bcf2e4256fab215e7008ab42
FredGSF <99353119+FredGSF@users.noreply.github.com>
2024-03-08 18:51:26 +0000

    Update Appwindow.py

--- Appwindow.py
+++ Appwindow.py
@@ -5,8 +5,9 @@ from winUpdateThread import UpdateThread
 import utils
 import json
 
-with open('config.json') as config_file:
+with open("config.json") as config_file:
     config = json.load(config_file)
+
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -19,9 +20,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setObjectName("MainWindow")
         self.setWindowTitle("Online Cheater Bot for chess.com")
         self.setEnabled(True)
-        self.resize(self.width, self.height+26)
-        self.setMinimumSize(QtCore.QSize(self.width, self.height+26))
-        self.setMaximumSize(QtCore.QSize(self.width, self.height+26))
+        self.resize(self.width, self.height + 26)
+        self.setMinimumSize(QtCore.QSize(self.width, self.height + 26))
+        self.setMaximumSize(QtCore.QSize(self.width, self.height + 26))
         self.setToolTip("")
         self.setToolTipDuration(0)
         self.setAutoFillBackground(False)
@@ -32,7 +33,9 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # main frame
         self.frame = QtWidgets.QFrame(self)
-        self.frame.setGeometry(QtCore.QRect(QtCore.QPoint(0, 0), QtCore.QPoint(self.width, self.height)))
+        self.frame.setGeometry(
+            QtCore.QRect(QtCore.QPoint(0, 0), QtCore.QPoint(self.width, self.height))
+        )
         self.frame.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
@@ -55,28 +58,44 @@ class MainWindow(QtWidgets.QMainWindow):
         self.boardSvgThread.boardSignal.connect(self.boardSvg.load)
 
     def BestMoveButton(self):
-        self.button = QPushButton('Hint', self.frame)
-        self.button.setGeometry(QtCore.QRect(QtCore.QPoint(round(0.2*self.width)+480,round(0.05*self.height)+40), 
-                                QtCore.QPoint(round(0.2*self.width)+600,round(0.05*self.height)+90)))
+        self.button = QPushButton("Hint", self.frame)
+        self.button.setGeometry(
+            QtCore.QRect(
+                QtCore.QPoint(
+                    round(0.2 * self.width) + 480, round(0.05 * self.height) + 40
+                ),
+                QtCore.QPoint(
+                    round(0.2 * self.width) + 600, round(0.05 * self.height) + 90
+                ),
+            )
+        )
         Font = QtGui.QFont("Helvetica", 12)
         Font.setBold(True)
         self.button.setFont(Font)
         self.button.clicked.connect(self.buttonEvent)
-        
+
     def BoardSVG(self):
         filename = "svg/empty_board.svg"
         self.boardSvg = QtSvg.QSvgWidget(filename, self.frame)
         self.boardSvg.setGeometry(
-            QtCore.QRect(QtCore.QPoint(round(0.2*self.width), round(0.05*self.height)+100), 
-                         QtCore.QPoint(round(0.2*self.width)+600, round(0.05*self.height)+700)))
+            QtCore.QRect(
+                QtCore.QPoint(round(0.2 * self.width), round(0.05 * self.height) + 100),
+                QtCore.QPoint(
+                    round(0.2 * self.width) + 600, round(0.05 * self.height) + 700
+                ),
+            )
+        )
         self.boardSvg.show()
 
     def EvalBar(self):
         # create a vertical progress bar
         self.bar = QProgressBar(self.frame)
         self.bar.setGeometry(
-            QtCore.QRect(QtCore.QPoint(round(0.05*self.width), round(0.05*self.height)), 
-                         QtCore.QPoint(round(0.05*self.width+50), round(0.95*self.height))))
+            QtCore.QRect(
+                QtCore.QPoint(round(0.05 * self.width), round(0.05 * self.height)),
+                QtCore.QPoint(round(0.05 * self.width + 50), round(0.95 * self.height)),
+            )
+        )
         self.bar.setValue(50)
         self.bar.setOrientation(QtCore.Qt.Vertical)
         self.bar.setTextVisible(False)
@@ -95,10 +114,15 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def EvalLabel(self):
         self.label = QLabel("0.0", self.frame)
-        self.label.move(round(0.05*self.width)+60, round(0.05*self.height))
+        self.label.move(round(0.05 * self.width) + 60, round(0.05 * self.height))
         self.label.setGeometry(
-                QtCore.QRect(QtCore.QPoint(round(0.05*self.width)+60, round(0.05*self.height)), 
-                             QtCore.QPoint(round(0.05*self.width)+200, round(0.05*self.height)+50)))
+            QtCore.QRect(
+                QtCore.QPoint(round(0.05 * self.width) + 60, round(0.05 * self.height)),
+                QtCore.QPoint(
+                    round(0.05 * self.width) + 200, round(0.05 * self.height) + 50
+                ),
+            )
+        )
         # set Font
         Font = QtGui.QFont("Helvetica", 25)
         Font.setBold(True)
@@ -113,24 +137,24 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.bar.setValue(100)
             else:
                 self.bar.setValue(0)
-                
-        else: # type == "cp"
-            eval_num = eval_value/100
+
+        else:  # type == "cp"
+            eval_num = eval_value / 100
             # change the label text
             self.label.setText(str(eval_num))
             # change the bar
-            if 50+(eval_num/8*50) > 100:
+            if 50 + (eval_num / 8 * 50) > 100:
                 val = 100
-            elif 50+(eval_num/8*50) < 0:
+            elif 50 + (eval_num / 8 * 50) < 0:
                 val = 0
             else:
-                val = round(50+(eval_num/8*50))
+                val = round(50 + (eval_num / 8 * 50))
             self.bar.setValue(val)
 
     def MainWindowScreenPosition(self):
         screen = QDesktopWidget().screenGeometry()
         W, H = screen.width(), screen.height()
-        self.move(W-self.width, 0)
+        self.move(W - self.width, 0)
 
     def setChessComponents(self, board, stockfish):
         self.stockfish = stockfish
@@ -150,4 +174,3 @@ if __name__ == "__main__":
     window.show()
 
     sys.exit(app.exec_())
-    

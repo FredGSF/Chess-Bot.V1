6badaa051a719365fcc6d2c834427d290eee0960
FredGSF <99353119+FredGSF@users.noreply.github.com>
2024-03-08 18:51:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,22 @@
 
 # Selenium
 from selenium import webdriver
+
 # GUI
 from PyQt5.QtWidgets import QApplication
 from AppWindow import MainWindow
+
 # Chess utilities
 from stockfish import Stockfish
 import chess
+
 # Extra functions
 import utils
 import sys
 from threading import Thread
 import json
 
-with open('config.json') as config_file:
+with open("config.json") as config_file:
     config = json.load(config_file)
 
 # create a Firefox geckodriver
@@ -31,12 +34,18 @@ window.show()
 # using stockfish engine
 ENGINE_PATH = utils.getStockfishEnginePath()
 stockfish = Stockfish(path=ENGINE_PATH, depth=config["engine"]["depth"])
-stockfish.update_engine_parameters({"Hash": config["engine"]["hash"], "Minimum Thinking Time": config["engine"]["min_time"]})
+stockfish.update_engine_parameters(
+    {
+        "Hash": config["engine"]["hash"],
+        "Minimum Thinking Time": config["engine"]["min_time"],
+    }
+)
 stockfish.set_elo_rating(config["engine"]["elo"])
 # creating a board from the chess library
 board = chess.Board()
 # making them accessible from the window
 window.setChessComponents(board, stockfish)
+
 
 def SeleniumFunction():
     print("Chess.com Bot Started...")
@@ -44,16 +53,20 @@ def SeleniumFunction():
 
     # detect the color of the player
     PLAYER_COLOR = None
-    white_king = utils.find_by_css_selector_persist(driver, "div[class=\"piece wk square-51\"]", wait=5)
-    black_king = utils.find_by_css_selector_persist(driver, "div[class=\"piece bk square-58\"]", wait=5)
-    if white_king.location['y'] > black_king.location['y']:
-        PLAYER_COLOR = 'W'
+    white_king = utils.find_by_css_selector_persist(
+        driver, 'div[class="piece wk square-51"]', wait=5
+    )
+    black_king = utils.find_by_css_selector_persist(
+        driver, 'div[class="piece bk square-58"]', wait=5
+    )
+    if white_king.location["y"] > black_king.location["y"]:
+        PLAYER_COLOR = "W"
     else:
-        PLAYER_COLOR = 'B'
+        PLAYER_COLOR = "B"
 
     while True:
         # next move's css
-        css_selector = f"div[data-ply=\"{MOVE_NUM}\"]"
+        css_selector = f'div[data-ply="{MOVE_NUM}"]'
 
         # wait for the player to make his move
         move = utils.find_by_css_selector_persist(driver, css_selector, wait=0.3).text
@@ -72,8 +85,9 @@ def SeleniumFunction():
         OutputFilename = utils.createSVGfromBoard(board)
         window.evalThread.updateEval(evaluation)
         window.boardSvgThread.updateBoard(OutputFilename)
-        
+
         MOVE_NUM += 1
+
 
 selenium_thread = Thread(target=SeleniumFunction)
 selenium_thread.start()
